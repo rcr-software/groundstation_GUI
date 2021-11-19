3f88c522ee554948fd7b1d342a92e28702f5982a
Jacob Higdon <jahigd02@louisville.edu>
2021-11-19 18:19:10 -0500

    first commit

---
+++ GUI_Driver.js
@@ -0,0 +1,67 @@
+//This is the first thing I have ever written in JavaScript. There will be boilerplate. It will be somewhat gross. I hope to have cleared up some of the muck with
+//loads of comments. E-mail me at jahigd02@louisville.edu (or tap on my shoulder if we're at the garage) if something stupid doesn't make sense.
+var trueSizeX;
+var trueSizeY;
+let edgeSmooth = 15;
+
+let dataPacket = [6];
+
+let packetTextFile = new File(["foo"], 'C:\\Users\\PMpun\\Documents\\Code\\RCR\\Project 1 - Python List Writing\\ListWriting_MKII_Python\\currentData.txt');
+let reader = new FileReader();
+
+class Pane { // 'Pane' object representing the Pane holding a GUI Element. Should be instantiated in setup().
+    constructor (x1, y1, width, height, title, titleXPad, titleYPad, titleFontSize) {
+      this.x1 = x1;
+      this.y1 = y1;
+      this.width = width;
+      this.height = height;
+      this.title = title;
+      this.titleXPad = titleXPad;
+      this.titleYPad = titleYPad;
+      this.titleFontSize = titleFontSize;
+    }
+    display()
+    {
+      noFill();
+      strokeWeight(5);
+      let c = color(240,55,107);
+      stroke(c);
+      rect(this.x1, this.y1, (this.width - this.x1), (this.height - this.y1), edgeSmooth); //Displays a physical (rounded) rectangle given the passed co-ordinates
+                                                                                           //from the user. Note that p5.js' (irritating) 3rd and 4th 
+                                                                                           //constructors for these shapes are width and height (not a 3rd/4th x/y vals)
+                                                                                           //so I wrote these operations to assume the user is passing a desired (x2,y2)
+                                                                                           //value, and subtracted this from the 1st (x1,y1) to give p5.js the width and
+                                                                                           //height it would want to map to our actual second coordinate.
+      noStroke();
+      fill(c);   
+      textSize(this.titleFontSize);                                                                                   
+      let titleText = this.title;
+      text(titleText, this.x1 + this.titleXPad, this.y1 + this.titleYPad);
+    }
+}
+
+function preload()
+{
+  trueSizeX = windowWidth; // "True Size" is the size element of the entire screen. Here, it's defined as the size of the window's width and height.
+                           // (this was done so desired screensize could be changed at the top of the script and have the effect cascade down throughout.)
+  trueSizeY = windowHeight;
+
+}
+function setup() {
+  createCanvas(trueSizeX, trueSizeY); // create the canvas with size to be truesizeX x truesizeY
+  solOnePane = new Pane(10, 10, 200, 200, 'Solenoid One', 20, 30, 25);
+  solTwoPane = new Pane(10,215,200,405, 'Solenoid Two', 20, 30, 25);
+  altPane = new Pane(215, 10, 800, 405, "Altitude", 250, 35, 25);
+
+}
+
+function draw() {
+  background(245);
+  solOnePane.display();
+  solTwoPane.display();
+  altPane.display();
+  altPane.x1 *= 1.001;
+  reader.readAsText(packetTextFile);
+  console.log(reader.result);
+}
+
