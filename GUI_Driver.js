fa6b39f53a9382b0caab4c0e4c941d45649988b1
Jacob Higdon <jahigd02@louisville.edu>
2021-11-30 21:59:15 -0500

    fetch 2

--- GUI_Driver.js
+++ GUI_Driver.js
@@ -52,7 +52,6 @@ function draw() {
   background(245);
   solOnePane.display();
   solTwoPane.display();
-  altPane.display();
   fetch("test.json")
   .then(response => response.json())
   .then(json => solTwoPane.title=json);
